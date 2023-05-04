78eb62881996493f786ef3198dff4ba433e20899
khemchand-twt11 <gauravsharma6290852476@gmail.com>
2023-05-04 21:05:38 +0530

    day-2 work

--- backend/index.js
+++ backend/index.js
@@ -7,6 +7,11 @@ const { connection } = require("./configs/db");
 const server = http.createServer(app);
 const io = require("socket.io")(server);
 
+//SOCKET LOGIC
+const { socketio } = require("./socket/socketio");
+
+socketio(io);
+
 //LISTEN
 const port = process.env.PORT || 3000;
 server.listen(port, async () => {
