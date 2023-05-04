e42491734ff047d82c818603bdd5b6d48f23baea
sandeepguptax2003 <sandeepguptax2003@gmail.com>
2023-05-04 21:13:26 +0530

    Added Index.html/main.css/script.js

---
+++ frontend/scripts/script.js
@@ -0,0 +1,9 @@
+// Hamburger menu JS starts
+
+function menuOnClick() {
+  document.getElementById("menu-bar").classList.toggle("change");
+  document.getElementById("nav").classList.toggle("change");
+  document.getElementById("menu-bg").classList.toggle("change-bg");
+}
+
+// Hamburger menu JS ends
