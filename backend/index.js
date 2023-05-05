c95739375b1530a42ef2d663816dcbfb15274ffc
khemchand-twt11 <gauravsharma6290852476@gmail.com>
2023-05-05 21:31:14 +0530

    day-3 work

--- backend/index.js
+++ backend/index.js
@@ -1,30 +1,53 @@
-// Load the required modules
 const express = require("express");
 const app = express();
 require("dotenv").config();
 const http = require("http");
-const { connection } = require("./configs/db");
 const server = http.createServer(app);
+const { connection, client } = require("./configs/db");
 const io = require("socket.io")(server);
-const {connection, client} = require("./configs/db");
-const {userrouter} = require("./routes/user.route");
+
+const { userRoute } = require("./routes/user.route");
 app.use(express.json());
+
 //SOCKET LOGIC
 const { socketio } = require("./socket/socketio");
 
 socketio(io);
 
+app.get("/", (req, res) => {
+  res.send("welcome to ChatMate home page");
+});
 
+//ROUTES
 
-//========================
+// app.use("/user", userRoute);
+app.post("/addContact", async (req, res) => {
+  try {
+    const contact = req.body; // Get the contact object from the request body
+    const result = await db.collection("contacts").insertOne(contact); // Add the contact to MongoDB
 
+    res.status(200).send(); // Send a success response
+  } catch (err) {
+    console.log(err);
+    res.status(500).send(); // Send an error response
+  }
+});
+// server side/backend code use this starts
 
+// API endpoint for searching contacts
+app.get("/search", (req, res) => {
+  const query = req.query.q.toLowerCase();
+  contacts.findOne({ name: { $regex: `^${query}` } }, (err, result) => {
+    if (err) throw err;
+    if (result) {
+      res.send(result.name);
+    } else {
+      res.send("");
+    }
+  });
+});
+// server side/backend code use this ends
 
-app.get("/",(req,res)=>{
-  res.send("welcome to ChatMate home page")
-})
-
-app.use("/user",userrouter);
 //LISTEN
 const port = process.env.PORT || 3000;
 server.listen(port, async () => {
@@ -35,4 +58,3 @@ server.listen(port, async () => {
   }
   console.log(`listening on *:${port}`);
 });
-
